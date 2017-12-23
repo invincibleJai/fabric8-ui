b78105a7d948220e8f9b6b79dc5b2924a8f5c21f
invinciblejai <mailjai.vardhan@gmail.com>
2017-12-23 12:29:32 +0530

    updates env
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

---
+++ environments/launcher-env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+
+export PATH=$PATH:~/openshift/
+export FABRIC8_FORGE_API_URL=http://localhost:8080/api/launchpad
+export WIT_URL=https://api.openshift.io
+export AUTH_URL=https://auth.openshift.io
+export KEYCLOAK_SAAS_URL=https://sso.openshift.io/
+export OPENSHIFT_API_URL=https://api.starter-us-east-2.openshift.com
