5d33857edd69501679374cf294fde8b78003840f
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2018-03-08 19:13:09 +0530

    feat(service): changes the mock data to accomodate the actual service

--- src/app/space/app-launcher/app-launcher.module.ts
+++ src/app/space/app-launcher/app-launcher.module.ts
@@ -2,10 +2,21 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Http } from '@angular/http';
 
+import {
+  Config,
+  HelperService,
+  TokenProvider
+} from 'ngx-forge';
+
+import { AuthenticationService } from 'ngx-login-client';
+
 import { AppLauncherRoutingModule } from './app-launcher-routing.module';
 import { AppLauncherComponent } from './app-launcher.component';
 import { CreateAppModule } from './create-app/create-app.module';
 import { ImportAppModule } from './import-app/import-app.module';
+
+import { KeycloakTokenProvider } from '../forge-wizard/service/token-provider';
+import { NewForgeConfig } from './shared/new-forge.config';
 
 @NgModule({
   imports: [
@@ -13,6 +24,15 @@ import { ImportAppModule } from './import-app/import-app.module';
     CommonModule,
     CreateAppModule,
     ImportAppModule
+  ],
+  providers: [
+    HelperService,
+    { provide: Config, useClass: NewForgeConfig },
+    {
+      provide: TokenProvider,
+      useFactory: (auth: AuthenticationService) => new KeycloakTokenProvider(auth),
+      deps: [AuthenticationService]
+    }
   ],
   declarations: [ AppLauncherComponent ]
 })
